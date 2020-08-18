9eb7043d26e797a7522d7e37fe15969d3d299cee
K-Winkles <kiana.villaera@senti.com.ph>
2020-08-18 10:16:46 +0800

    Updated to version 0.7 and rebuilt package

--- build/lib/dcutils/move_batch.py
+++ build/lib/dcutils/move_batch.py
@@ -6,13 +6,16 @@ def mv_batch(config, SRC_BUCKET, DEST_BUCKET, DEST_BLOB, PARENT_FOLDER):
     STORAGE_CLIENT = storage.Client.from_service_account_json(config)
 
     blobs = STORAGE_CLIENT.list_blobs(SRC_BUCKET)
-    for blob in blobs:
-        folders = blob.name.split('/')
-        if folders[0] == 'datasets':
-            print('working on {}'.format(blob.name))
-            mv_blob(
-                STORAGE_CLIENT,
-                blob.name, DEST_BLOB + '/' + blob.name,
-                SRC_BUCKET,
-                DEST_BUCKET
-            )
+    print('starting move job...')
+    try:
+        for blob in blobs:
+            folders = blob.name.split('/')
+            if folders[0] == 'datasets':
+                mv_blob(
+                    STORAGE_CLIENT,
+                    blob.name, DEST_BLOB + '/' + blob.name,
+                    SRC_BUCKET,
+                    DEST_BUCKET
+                )
+    except ConnectionError as e:
+        print('Error: {}'.format(e))
