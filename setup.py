d61124d71fcb955cd47de7d63592338606f7d4ae
K-Winkles <kiana.villaera@senti.com.ph>
2020-11-13 19:48:55 +0800

    version 0.29

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dcutils",
-    version="0.28",
+    version="0.29",
     author="Kiana Alessandra V. Villaera",
     author_email="kiana.villaera@senti.com.ph",
     description="A simple package for data center utilities",
